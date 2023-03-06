3c6cebfda41381f07e6d2a9282b3782572a74728
Omar Khankan <omar.khankan@gmail.com>
2023-03-06 16:25:16 +0200

    File Structuring

---
+++ lms-frontend/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Scroll from "./components/Home/ScrolltoTop/Scroll";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ lms-frontend/src/components/Home/404 not Found/PagenotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './PagenotFound.css'
+
+
+function PagenotFound() {
+  return (
+    <div className='pagenotfound'>
+      <h1 className='notfound'>404</h1>
+      <br></br>
+      <p>Page Not Found</p>
+    </div>
+  )
+}
+
+export default PagenotFound
