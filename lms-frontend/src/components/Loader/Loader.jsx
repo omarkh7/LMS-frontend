d80a331f4e5577615fd57ff21b36a62c41e163e4
nour <nour.hmadi.a@gmail.com>
2023-03-10 14:04:12 +0200

    loader component

--- lms-frontend/src/components/Loader/Loader.jsx
+++ lms-frontend/src/components/Loader/Loader.jsx
@@ -1,22 +1,13 @@
-import React from 'react'
+import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import LinearProgress from '@mui/material/LinearProgress';
 
-
-
-function Loader() {
+export default function LinearColor() {
   return (
-    <div>
-       <Stack sx={{ width: '100%', color: 'grey.500' }} spacing={2}>
+    <Stack sx={{ width: '100%', color: 'grey.500' }} spacing={2}>
       <LinearProgress color="secondary" />
       <LinearProgress color="success" />
       <LinearProgress color="inherit" />
     </Stack>
-    </div>
-  )
+  );
 }
-
-export default Loader
-
-
-
